e1ef2c1a436e5d7d14e8e61eb40ebd9467140333
MilkMan127 <4394348@myuwc.ac.za>
2026-03-02 14:50:02 +0200

    Implement hash function and insert method

--- openHash.java
+++ openHash.java
@@ -12,3 +12,36 @@ public class openHash {
         values = new String[m];
         size = 0;
     }
+ public int hash(String key) {
+
+        int hash = 0;
+
+        for (int i = 0; i < key.length(); i++) {
+            hash = (31 * hash + key.charAt(i)) % m;
+        }
+
+        return Math.abs(hash);
+    }
+    public void insert(String key, String value) {
+
+        if (isFull())
+            return;
+
+        int i = hash(key);
+
+        // Linear probing
+        while (keys[i] != null && !keys[i].equals("DELETED")) {
+
+            if (keys[i].equals(key)) {
+                values[i] = value; // update existing
+                return;
+            }
+
+            i = (i + 1) % m;
+        }
+
+        keys[i] = key;
+        values[i] = value;
+        size++;
+    }
+
