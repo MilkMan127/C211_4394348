81368c83dfe3e10646199ccda76ea5a09a799681
MilkMan127 <4394348@myuwc.ac.za>
2026-03-02 15:20:10 +0200

    Implement openHash class with constructor
    Added openHash class with constructor and fields.

--- Expirement.java
+++ Expirement.java
@@ -1,1 +1,15 @@
+public class openHash {
+
+    private String[] keys;
+    private String[] values;
+    private int m;          // table size
+    private int size;       // number of elements stored
+
+    // Constructor
+    public openHash(int tableSize) {
+        m = tableSize;
+        keys = new String[m];
+        values = new String[m];
+        size = 0;
+    }
 
