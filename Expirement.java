f08f991f68f9b75ee02b1b8e8fe92f6b54b7c6f6
MilkMan127 <4394348@myuwc.ac.za>
2026-03-02 15:21:53 +0200

    Implement insert method in Expirement class
    Add insert method for key-value pairs with linear probing.

--- Expirement.java
+++ Expirement.java
@@ -22,3 +22,25 @@ public int hash(String key) {
 
         return Math.abs(hash);
     }
+     public void insert(String key, String value) {
+
+        if (isFull())
+            return;
+
+        int i = hash(key);
+
+        // Linear probing
+        while (keys[i] != null && !keys[i].equals("DELETED")) {
+
+            if (keys[i].equals(key)) {
+                values[i] = value; // update existing
+                return;
+            }
+
+            i = (i + 1) % m;
+        }
+
+        keys[i] = key;
+        values[i] = value;
+        size++;
+    }
