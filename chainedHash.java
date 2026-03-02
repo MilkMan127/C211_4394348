7c9869c2a2a90e610ac21def28a710b16bae7984
MilkMan127 <4394348@myuwc.ac.za>
2026-03-02 15:07:45 +0200

    Add initial implementation of chainedHash class
    Implement the initial structure of the chainedHash class with a nested Node class and constructor.

--- chainedHash.java
+++ chainedHash.java
@@ -1,1 +1,25 @@
+ublic class chainedHash {
 
+    // Node for linked list
+    private class Node {
+        String key;
+        String value;
+        Node next;
+
+        Node(String key, String value) {
+            this.key = key;
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    private Node[] table;
+    private int m;      // table size
+    private int size;   // number of key-value pairs
+
+    // Constructor
+    public chainedHash(int m) {
+        this.m = m;
+        table = new Node[m];
+        size = 0;
+    }
