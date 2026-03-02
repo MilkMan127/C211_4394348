cc323b0a8c1da1882de7b23d825162ee0c8140a3
MilkMan127 <4394348@myuwc.ac.za>
2026-03-02 15:20:42 +0200

    Add hash function to Expirement class
    Implement hash function for string keys

--- Expirement.java
+++ Expirement.java
@@ -12,4 +12,13 @@ public class openHash {
         values = new String[m];
         size = 0;
     }
+public int hash(String key) {
 
+        int hash = 0;
+
+        for (int i = 0; i < key.length(); i++) {
+            hash = (31 * hash + key.charAt(i)) % m;
+        }
+
+        return Math.abs(hash);
+    }
